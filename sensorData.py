1075ac2bad58d74f4187073e5e6e394bae106b73
Eyal Golan <eyalgolan96@gmail.com>
2020-12-06 22:57:36 +0200

    basic implementation of json building functions. need to test this

--- sensorData.py
+++ sensorData.py
@@ -14,9 +14,47 @@ class SensorData:
         self.raw_json = self.get_rawdata_json()
         self.sensor_dataframe = self.get_sensor_data()
 
+    def get_filename(self, in_filename, file_type):
+        for fname in os.listdir(self.raw_data_dir):
+            if in_filename in fname and file_type in fname:
+                return str(fname)
+
     def get_rawdata_json(self):
-        
+        name = self.raw_data_dir
+
+        metis = {"events": self.get_filename("events", "csv"),
+                 "sensors": self.get_filename("sensors", "csv")}
+        sensors ={"OH1":
+                      {"ACC":{"dir":"",
+                              "files":[self.get_filename("ACC", "txt")]},
+                       "PPG":{"dir":"",
+                              "files":[self.get_filename("PPG", "txt")]}},
+                  "H10":
+                      {"ECG":{"dir":"",
+                              "files":[self.get_filename("ECG", "txt")]},
+                       "ACC":{"dir":"",
+                              "files":[self.get_filename("ACC", "txt")]}}}
+        videos = {"phone":
+                      {"dir":"phone",
+                       "files":[self.get_filename("P", "mp4")]},
+                  "face":
+                      {"dir":"phone",
+                       "files":""},
+                  "body":
+                      {"dir":"phone",
+                       "files":""},
+                  "face_gopro":
+                      {"dir":"phone",
+                       "files":""}}
+
+        data = {"name":name,
+                "metis":metis,
+                "sensors":sensors,
+                "videos":videos}
+
+        raw_json = json.loads(data)
         return raw_json
+
     def get_sensor_data(self):
         n_sensors = len(self.raw_json["sensors"])
 
